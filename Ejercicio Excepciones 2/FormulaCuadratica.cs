cc70fc479a22972db29d362f9950d88bccabd24b
gurdian0614 <ivan.gurdian@outlook.com>
2024-02-07 23:32:36 -0600

    Add project files.

---
+++ Ejercicio Excepciones 2/FormulaCuadratica.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Ejercicio_Excepciones_2
+{
+    public class FormulaCuadratica
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public string Calcular()
+        {
+            if (A != 0)
+            {
+                double resultadoRaiz = Math.Pow(B, 2) - (4 * A * C);
+
+                if (resultadoRaiz >= 0)
+                {
+                    double x1 = (-B + Math.Sqrt(resultadoRaiz)) / (2 * A);
+                    double x2 = (-B - Math.Sqrt(resultadoRaiz)) / (2 * A);
+                    
+                    return $"X1={x1}, X2={x2}";
+                } else
+                {
+                    return "La raíz cuadrada no puede ser negativa";
+                }
+
+            } else
+            {
+                return "No se puede dividir entre cero";
+            }
+        }
+    }
+}
---
+++ Ejercicio Excepciones 2/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using Ejercicio_Excepciones_2;
+
+try
+{
+    double a, b, c;
+    string sa, sb, sc;
+    FormulaCuadratica fm = new FormulaCuadratica();
+
+    Console.WriteLine("Ingrese valor a:");
+    sa = Console.ReadLine();
+    if (double.TryParse(sa, out a) == true)
+    {
+        fm.A = a;
+        Console.WriteLine("Ingrese valor b:");
+        sb = Console.ReadLine();
+
+        if (double.TryParse(sb, out b) == true)
+        {
+            fm.B = b;
+            Console.WriteLine("Ingrese valor c:");
+            sc = Console.ReadLine();
+            if (double.TryParse(sc, out c) == true)
+            {
+                fm.C = c;
+                Console.WriteLine(fm.Calcular());
+            } else
+            {
+                Console.WriteLine($"{sc} no es un valor numérico");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"{sb} no es un valor numérico");
+        }
+    } else
+    {
+        Console.WriteLine($"{sa} no es un valor numérico");
+    }
+} catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
